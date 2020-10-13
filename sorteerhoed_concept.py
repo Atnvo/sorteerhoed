8c96386daeecb3edf9c77b35a68b55998c285752
atnvo <alavi.vano@gmail.com>
2020-10-13 11:16:29 +0200

    Concept toegevoegd

---
+++ sorteerhoed_concept.py
@@ -0,0 +1,46 @@
+import ast
+
+def main():
+    while True:
+        try:
+            x = input("MENU Selecteer een keuze \nq: quit\n1: vragenlijst \n2: laatste uitslag tonen\nkeuze: ")
+            if (x == 'q'): 
+                exit()
+            if (x == '1'):
+                vraag_en_antwoord(vragen_ophalen())
+            if (x == '2'):  
+                toon_resultaten()
+        except (ValueError, FileNotFoundError):
+            print("Oops! Bestand niet gevonden.  Probeer opnieuw...")
+
+def vragen_ophalen():                                           #haal de vragen op uit de tekst bestand 
+    with open('meerkeuzevragen.txt', 'r') as file:              #Open de text bestand
+        contents = file.read()                                  #sla de daqta op in een variable
+        data = ast.literal_eval(contents)                       #zet de data om in een dictionary
+        file.close()                                            #sluit de file
+    return data                                                 #stuur de data terug
+
+def vraag_en_antwoord(data_type):
+    ingevoerde_antwoord = {}
+    for key, value in data_type.items():
+        print(str(key))                                         #print de vraag
+        for nummer, x in value.items():                         #loop door de nummer en antwoord
+            print(' '+ str(nummer) + ": " + str(x))             #print de nummer en antwoord optie
+        try:
+            ingevoerde_antwoord[str(key)] = input('Antwoord: ')
+        except (ValueError):
+            print("Oops! verkeerd invoer.  Probeer opnieuw...")
+
+    with open('antwoorden_gebruiker.txt', 'w') as f:            #open de antwoorde file
+        f.write(str(ingevoerde_antwoord))                       #schrijf de antwoorden in de file
+        # for ant in ingevoerde_antwoord:
+        f.close()   
+
+def toon_resultaten():                                          #Toon de resultaten van de quiz
+    with open('antwoorden_gebruiker.txt', 'r') as file:         #Open de text bestand
+        contents = file.read()                                  #Lees de inhoud en sla het in een variable op   
+        file.close()                                            #sluit de file
+    print(str(contents))                                        #print de resultaat nqar de console
+
+if __name__ == "__main__":
+    main()
---
+++ test_sorteerhoed_concept.py
@@ -0,0 +1,5 @@
+import sorteerhoed_concept as sort
+
+print(sort.vragen_ophalen())
+# sort.main()
+sort.vraag_en_antwoord()
