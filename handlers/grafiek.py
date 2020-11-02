a652d2af1fe9a7c1d7fa9772bdf3861013b5eaef
KrisNibbelink <72446103+KrisNibbelink@users.noreply.github.com>
2020-11-02 19:41:31 +0100

    nu

--- handlers/grafiek.py
+++ handlers/grafiek.py
@@ -4,9 +4,10 @@ import matplotlib.pyplot as plt
 def pi(resultaten):
     # Pie chart, where the slices will be ordered and plotted counter-clockwise:
     labels = 'IAT', 'FICT', 'BDAM', 'SE'
-    sizes = resultaten['iat'], resultaten['fict'], resultaten['se'], resultaten['bdam']
-    explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
-
+    print(int(resultaten['iat']), int(resultaten['fict']), int(resultaten['se']), int(resultaten['bdam']))
+    sizes = int(resultaten['iat']), int(resultaten['fict']), int(resultaten['se']), int(resultaten['bdam'])
+#     sizes = [10,50,20,30]
+    explode = (0, 0, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
     fig1, ax1 = plt.subplots()
     ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
             shadow=True, startangle=90)
